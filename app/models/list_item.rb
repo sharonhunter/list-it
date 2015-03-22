901549329379cb80f973f0e52724309d6847e546
Sharon Hunter <sharonhunter@runbox.com>
2015-03-22 13:12:50 -0400

    Fixes div reordering in list#show; displays by created_at.

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,5 +1,9 @@
 class ListItem < ActiveRecord::Base
   belongs_to :list
+  #line 6 added to fix odd behavior where list item 
+  #divs would reorder upon completion in list#show
+  #line 6 adds new item to top of list and keeps in place correctly for delet or complete
+  default_scope { order('created_at DESC') }
 
   def completed?
   	!completed_at.blank?
