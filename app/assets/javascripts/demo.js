ef307f3b6edcc606fc73f5fd55bd5b997bb547f3
Sharon Hunter <sharonhunter@runbox.com>
2015-04-05 11:35:28 -0400

    Adds event delegation and ability to add dynamic list items to demo.

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,19 +1,31 @@
 $(document).ready(function(){
+
+	function crossOutItem(){
+		$('table').on('click', 'tr td.icon-cell-remove', function(event){
+			$(this).children('a').removeClass("glyphicon-remove").addClass("glyphicon-ok");
+			$(this).filter('.icon-cell-remove').next().addClass("cross-out");
+		})
+	}
+
 	function deleteItem(){
-		$("td.icon-cell-trash").on("click", function(){
+		$('table').on('click', 'tr td.icon-cell-trash', function(event){
 			alert("Are you sure?");
 			$(this).closest('tr').remove();
 		})
 	}
 
-	function crossOutItem(){
-		$("td.icon-cell-remove").on("click", function(){
-			$(this).children('a').removeClass("glyphicon-remove").addClass("glyphicon-ok");
-			$(this).filter(".icon-cell-remove").next().addClass("cross-out");
+	function addListItem(){
+		//var userInput = $('#demo-list-item').val();
+		$('#demo-submit-btn').on('click', function(event){
+			$('#demo-list').prepend("<tr> + <td class='icon-cell-remove'><a class='glyphicon glyphicon-remove'</a></td>" + 
+				"<td class='item-cell text-left'>" + $('#demo-list-item').val() + "</td>" + "<td class='icon-cell-trash'><a class='glyphicon glyphicon-trash'</a></td></tr>");
+			$('#demo-list-item').val('');
 		})
 	}
 
-	deleteItem();
 	crossOutItem();
+	deleteItem();
+	addListItem();
+
 });
 
